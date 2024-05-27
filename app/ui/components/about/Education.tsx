6eeaa8cc72f2c3061da32baeb2d3bea8a51a9e8a
Lwin Maung Maung <lwinmaungmaung.ytu@gmail.com>
2024-05-27 23:45:15 +0630

    Update order in exp and edu

--- app/ui/components/about/Education.tsx
+++ app/ui/components/about/Education.tsx
@@ -4,16 +4,16 @@ export default function Education(){
     return (
         <div className="space-y-3 p-10 rounded-3xl bg-slate-100 dark:bg-slate-800">
             <EducationItem
-            name="Mandalay Technological University"
-            title="Bachelor of Engineering, BE. Mechatronics Engineering"
-            startDate={new Date("2013/12/01")}
-            endDate={new Date("2015/10/01")}
-            />
-            <EducationItem
             name="Yangon Technological University"
             title="Bachelor of Engineering, BE. Mechatronics Engineering"
             startDate={new Date("2015/12/01")}
             endDate={new Date("2019/10/01")}
+            />
+            <EducationItem
+            name="Mandalay Technological University"
+            title="Bachelor of Engineering, BE. Mechatronics Engineering"
+            startDate={new Date("2013/12/01")}
+            endDate={new Date("2015/10/01")}
             />
         </div>
     );
--- app/ui/components/about/Experience.tsx
+++ app/ui/components/about/Experience.tsx
@@ -4,11 +4,11 @@ export default function Experience(){
     return (
         <div className="space-y-3 p-10 rounded-3xl bg-slate-100 dark:bg-slate-800">
             <ExperienceItem
-            name="Software Developer"
-            title="Cosmos Solutions & Services"
+            name="Software Engineer"
+            title="Frontiir Co., Ltd"
             location="Yangon, Myanmar"
-            startDate={new Date("2019/11/01")}
-            endDate={new Date("2020/04/30")}
+            startDate={new Date("2021/01/18")}
+            endDate={new Date("2024/03/18")}
             />
             <ExperienceItem
             name="System and Network Engineer"
@@ -18,11 +18,11 @@ export default function Experience(){
             endDate={new Date("2020/12/30")}
             />
             <ExperienceItem
-            name="Software Engineer"
-            title="Frontiir Co., Ltd"
+            name="Software Developer"
+            title="Cosmos Solutions & Services"
             location="Yangon, Myanmar"
-            startDate={new Date("2021/01/18")}
-            endDate={new Date("2024/03/18")}
+            startDate={new Date("2019/11/01")}
+            endDate={new Date("2020/04/30")}
             />
         </div>
     );
