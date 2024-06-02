e8886035d6772dd4e9928ee18098de6fe72f14ad
Lwin Maung Maung <lwinmaungmaung.ytu@gmail.com>
2024-06-02 21:50:09 +0630

    Fixes profile image

--- app/ui/sections/AboutSection.tsx
+++ app/ui/sections/AboutSection.tsx
@@ -12,9 +12,10 @@ export default function AboutSection() {
             <div>
                 <h2 className="text-4xl font-bold mb-4">About Me</h2>
                 <div>
-                  <div className="relative float-left mx-2 rounded-full bg-slate-100 dark:bg-slate-800 w-[150px] h-[150px]">
+                  <div className="relative float-left mx-2 rounded-full bg-slate-100 dark:bg-slate-800 w-[110px] h-[110px] lg:w-[150px] lg:h-[150px]">
                     <Link href="https://www.linkedin.com/in/lwinmgmg/" target="_blank">
-                      <Image src="/images/profile.png" className="absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" height={125} width={125} alt="Profile" />
+                      <Image src="/images/profile.png" className="absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2 hidden lg:block" height={125} width={125} alt="Profile" />
+                      <Image src="/images/profile.png" className="absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2 block lg:hidden" height={100} width={100} alt="Profile" />
                     </Link>
                   </div>
                   <p className="text-lg mb-4 text-justify">Highly motivated Senior Software Engineer seeking a Backend or Full-Stack development role with expertise in cloud computing (including containerization with Docker and Kubernetes) and experience in various programming languages (Python, Golang).
