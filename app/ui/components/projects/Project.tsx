0757a0b2e071fd0dfeaa8f6dbfbedb86c0b04832
Lwin Maung Maung <lwinmaungmaung.ytu@gmail.com>
2024-06-03 14:10:15 +0630

    Added project item

--- app/ui/components/projects/Project.tsx
+++ app/ui/components/projects/Project.tsx
@@ -8,12 +8,17 @@ imageUrl = "/images/pp.png"
     imageUrl?: string
 }){
     return (
-        <div className="max-w-sm border-2 rounded-xl border-slate-600 dark:border-slate-300 bg-slate-100 dark:bg-slate-800 p-5">
-            <div className="h-32">
-
+        <div className="max-w-sm shadow-sm hover:shadow-slate-600 rounded-xl bg-slate-100 dark:bg-slate-800">
+            <div className="h-40 w-full relative rounded-xl shadow-lg hover:shadow-slate-600 group" style={{
+                background: `url(${imageUrl})`,
+                backgroundSize: "cover"
+            }}>
+                <div className="overlay absolute top-0 left-0 rounded-xl h-full w-full bg-slate-200 dark:bg-slate-800 bg-opacity-0 hidden group-hover:flex group-hover:bg-opacity-50 transition-all duration-500"></div>
             </div>
-            <h3 className="text-lg font-semibold">{title}</h3>
-            <p className="text-sm">{detail}</p>
+            <div className="p-5">
+                <h3 className="text-lg font-semibold">{title}</h3>
+                <p className="text-sm">{detail}</p>
+            </div>
         </div>
     );
 }
