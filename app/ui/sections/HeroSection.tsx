d5de316139f16eb1b9c58b2387e8bb0f9cda2113
Lwin Maung Maung <lwinmaungmaung.ytu@gmail.com>
2024-08-20 10:30:35 +0630

    Added email in hero section

--- app/ui/sections/HeroSection.tsx
+++ app/ui/sections/HeroSection.tsx
@@ -16,7 +16,7 @@ export default function HeroSection(){
                     </h1>
                     <p className="text-lg lg:text-xl ">Thank you for visiting my Portfolio Website. If you&apos;d like to contribute the design and the source code, please visit <a className="text-blue-500 dark:text-blue-300 underline capitalize" href="https://github.com/lwinmgmg/portfolio" target="_blank">here</a>.</p>
                     <div className="space-y-3 lg:space-x-3 mt-3">
-                        <button className="px-6 py-3 rounded-full w-full lg:w-fit text-white bg-gradient-to-r from-blue-400 to-green-400 hover:from-green-400 hover:to-blue-400">Hire Me</button>
+                        <a href="mailto:lwinmaungmaung.ytu@gmail.com"><button className="px-6 py-3 rounded-full w-full lg:w-fit text-white bg-gradient-to-r from-blue-400 to-green-400 hover:from-green-400 hover:to-blue-400">Hire Me</button></a>
                         <button className="px-6 py-3 rounded-full w-full lg:w-fit dark:bg-transparent border border-black hover:bg-slate-400 dark:text-white dark:bg-slate-800 dark:border-white dark:hover:bg-slate-800"><Link href={"/attachments/lmm_resume.pdf"} target="_blanc">Download Resume</Link></button>
                     </div>
                 </div>
